616231a3f816b0049bbc7d16ee5da2b184583ca3
Valvar-N <yigitelarslan3@gmail.com>
2025-05-14 21:04:52 +0300

    utils file formed

--- data_preprocess.py
+++ data_preprocess.py
@@ -3,55 +3,75 @@ import numpy as np
 import matplotlib.pyplot as plt
 from haversine import haversine, Unit
 import os
-from geopy.distance import geodesic
+import utils
 
-# Get the directory of the current Python file
-current_dir = os.path.dirname(os.path.abspath(__file__))
-folder_path = os.path.join(current_dir, 'raw_datas')
+# List of csv file numbers pulled from log files of UAV
+list_of_files = [
+    67,
+    68,
+    69,
+    79,
+    81,
+    86,
+    88,
+    89,
+    90,
+    102,
+    106,
+    109,
+    111
+]
 
-# Specify the filename
-filename = '67.csv'
+for item in list_of_files:
+    file_path = utils.find_file_path("raw_datas", f"{item}.csv")
+    # Load your data
+    df = pd.read_csv(file_path)
 
-# Get the full path to the file
-file_path = os.path.join(folder_path, filename)
+    # Convert the 'timestamp(ms)' column to seconds
+    df["time_s"] = (df["timestamp(ms)"].iloc[0]) / 1000.0
+    df["time_diff"] = (df["timestamp(ms)"] - df["timestamp(ms)"].iloc[0]) / 1000.0
 
-# Load your data
-df = pd.read_csv(file_path)
+    # Normalize gps data
+    df["lat"] = df["GPS[0].Lat"] / 1e7
+    df["lon"] = df["GPS[0].Lng"] / 1e7
 
-# Convert the 'timestamp(ms)' column to seconds
-df['time_s'] = (df['timestamp(ms)'].iloc[0]) / 1000.0
-df['time_diff'] = (df['timestamp(ms)'] - df['timestamp(ms)'].iloc[0]) / 1000.0
+    # Clear noise in the data
+    df["speed"] = df["GPS[0].Spd"].rolling(window=5).mean()
+    df["roll"] = df["ATT.Roll"].rolling(window=5).mean()
+    df["yaw"] = df["AHR2.Yaw"].rolling(window=5).mean()
 
-# Clear noise in the data
-df['speed'] = df['GPS[0].Spd'].rolling(window=5).mean()
-df['roll'] = df['ATT.Roll'].rolling(window=5).mean()
-df['yaw'] = df['AHR2.Yaw'].rolling(window=5).mean()
+    # After clearing noise, drop the rows with NaN values
+    df.dropna(inplace=True)
+    df.reset_index(drop=True, inplace=True)
 
-# After clearing noise, drop the rows with NaN values
-df.dropna(inplace=True)
-df.reset_index(drop=True, inplace=True)
+    df["yaw_rad"] = np.deg2rad(df["yaw"])
+    df["yaw_unwrapped"] = np.unwrap(df["yaw_rad"])  # fix 360 → 0 wrap
 
-df['yaw_rad'] = np.deg2rad(df['yaw'])
-df['yaw_unwrapped'] = np.unwrap(df['yaw_rad'])  # fix 360 → 0 wrap
+    df["yaw_rate"] = df["yaw_unwrapped"].diff() / df["time_diff"].diff()
+    df["yaw_rate_deg"] = np.rad2deg(df["yaw_rate"])
 
-df['yaw_rate'] = df['yaw_unwrapped'].diff() / df['time_diff'].diff()
-df['yaw_rate_deg'] = np.rad2deg(df['yaw_rate'])
+    df.dropna(inplace=True)
+    df.reset_index(drop=True, inplace=True)
 
-df.dropna(inplace=True)
-df.reset_index(drop=True, inplace=True)
+    distances = [0]
+    for i in range(1, len(df)):
+        dist = utils.haversine(df.iloc[i], df.iloc[i - 1])
+        distances.append(dist)
 
-def calc_distance(row1, row2):
-    return geodesic((row1['GPS[0].Lat'], row1['GPS[0].Lng']), (row2['GPS[0].Lat'], row2['GPS[0].Lng'])).meters
+    df["distance_m"] = distances
+    df["total_distance"] = df["distance_m"].cumsum()
 
-distances = [0]
-for i in range(1, len(df)):
-    dist = calc_distance(df.iloc[i], df.iloc[i - 1])
-    distances.append(dist)
+    df.drop(
+        columns=[
+            "timestamp(ms)",
+            "GPS[0].Lat",
+            "GPS[0].Lng",
+            "GPS[0].Spd",
+            "ATT.Roll",
+            "AHR2.Yaw",
+        ],
+        inplace=True,
+    )
+    df.dropna(inplace=True)
 
-df['distance_m'] = distances
-df['total_distance'] = df['distance_m'].cumsum()
-
-df.to_csv('processed_data.csv', index=False)
-
-# TODO setup module for more readbility and maintainability
-# TODO continue preprocessing for distance 
+    df.to_csv(f"processed_{item}.csv", index=False)
---
+++ utils.py
@@ -0,0 +1,31 @@
+import os
+from geopy.distance import geodesic
+import numpy as np
+
+
+def find_file_path(target_folder, file_name):
+    # Get the directory of the current Python file
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    folder_path = os.path.join(current_dir, target_folder)
+    # Get the full path to the file
+    file_path = os.path.join(folder_path, file_name)
+    return file_path
+
+
+def calc_distance(row1, row2):
+    return geodesic(
+        (row1["lat"], row1["lon"]),
+        (row2["lat"], row2["lon"]),
+    ).meters
+
+
+def haversine(row1, row2):
+    R = 6371000  # Radius of Earth in meters
+    lat1, lon1 = np.radians([row1["lat"], row1["lon"]])
+    lat2, lon2 = np.radians([row2["lat"], row2["lon"]])
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+
+    a = np.sin(dlat / 2) ** 2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon / 2) ** 2
+    c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
+    return R * c
